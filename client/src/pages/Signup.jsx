16de7dd8ce29d1ef91420fa0a7a0914592e6e6b1
davina708 <davinida96@gmail.com>
2022-12-23 17:02:29 +0900

    [FE]feat: Signup page 구현시작

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,5 +1,16 @@
 import React from "react";
-import { useForm } from "react-hook-form";
+import styled from "styled-components";
+
+const SignupBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  background-color: #f1f2f4;
+  min-height: 100vh;
+`;
+
 export default function Signup() {
-  return <div>Signup</div>;
+  return <SignupBox></SignupBox>;
 }
